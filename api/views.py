6b167bc99bbd9d9969c36a022977ae7f682fb529
WekesaDaniel <danielwekesakakai@gmail.com>
2024-05-24 02:41:51 -0400

    First commit

---
+++ api/views.py
@@ -0,0 +1,10 @@
+'''from django.shortcuts import render
+
+
+#TODO: create react app - frontend
+#simple API Endpoint
+# Create your views here.
+from django.http import JsonResponse
+
+def hello(request):
+    return JsonResponse({'message': 'Hello from Django API!'})'''
